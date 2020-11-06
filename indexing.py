3db3a5ab22152846e9f6c6cf94fd4613301f72f4
federicogibertoni <fede.gibbe@gmail.com>
2020-11-06 18:02:17 +0100

    lettura intera directory

--- indexing.py
+++ indexing.py
@@ -1,9 +1,5 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Oct 28 21:05:03 2020
-
-@author: Mattia
-"""
+from os import listdir
+from os.path import isfile, join
 
 from whoosh.index import create_in
 from whoosh.fields import *
@@ -11,27 +7,12 @@ from whoosh.fields import *
 schema = Schema(title=TEXT(stored=True), url=ID(stored=True), content=TEXT)
 ix = create_in("indexdir", schema)
 
+filelist = [f for f in listdir("corpus") if isfile(join("corpus", f))]
 
 writer = ix.writer()
 
-for i in range(0,40):
-    f=open(f"corpus/butac_{i}.txt","r", encoding='utf-8')
-    furl=f.readline()
-    furl=furl.rstrip()
-    
-    ftitle=f.readline()
-    ftitle=ftitle.rstrip()
-    
-    fkeyword=f.readline()
-    fkeyword=fkeyword.rstrip()
-    
-    fcontent =f.read()
-    
-    writer.add_document(title=ftitle, url=furl,content=fcontent)
-    f.close()
-
-for i in range(0, 39):
-    f = open(f"corpus/bufale_{i}.txt", "r", encoding='utf-8')
+for i in filelist:
+    f = open("corpus/" + i, "r", encoding='utf-8')
     furl = f.readline()
     furl = furl.rstrip()
 
@@ -47,5 +28,3 @@ for i in range(0, 39):
     f.close()
 
 writer.commit()
-
-
